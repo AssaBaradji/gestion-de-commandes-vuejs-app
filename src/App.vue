6bfd58ccaa92b6c9d4721489e244edb4d18e741a
AssaBaradji <baradjiassa695@gmail.com>
2024-09-23 09:50:27 +0200

    correction du menu actif

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
           <div class="d-flex align-items-center mx-4">
             <i class="fa-solid fa-users"></i>
             <router-link
-              class="nav-link"
-              :to="{ name: 'customers' }"
+              :class="['nav-link' , { active: activeRoute === '/customers'}]"
+              :to="{ name: 'customers' }" @click="setActiveRoute('/customers')"
               exact-active-class="active"
             >
               Customers
@@ -17,8 +17,8 @@
           <div class="d-flex align-items-center mx-4">
             <i class="fa-solid fa-box-open"></i>
             <router-link
-              class="nav-link"
-              :to="{ name: 'products' }"
+              :class="['nav-link' , { active: activeRoute === '/products'}]"
+              :to="{ name: 'products' }" @click="setActiveRoute('/products')"
               exact-active-class="active"
             >
               Products
@@ -28,8 +28,8 @@
           <div class="d-flex align-items-center mx-4">
             <i class="fa-solid fa-cart-shopping"></i>
             <router-link
-              class="nav-link"
-              :to="{ name: 'orders' }"
+              :class="['nav-link' , { active: activeRoute === '/orders'}]"
+              :to="{ name: 'orders' }" @click="setActiveRoute('/orders')"
               exact-active-class="active"
             >
               Orders
@@ -47,3 +47,16 @@
   color: blue !important;
 }
 </style>
+<script setup>
+import { ref } from "vue";
+import { RouterLink, RouterView, useRoute } from "vue-router";
+
+const activeRoute = ref("");
+const route = useRoute();
+
+const setActiveRoute = (path) => {
+  activeRoute.value = path;
+};
+
+activeRoute.value = route.path;
+</script>
